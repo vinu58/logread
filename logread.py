b731570db5d8442c10d2d199ac09ef565d95adbf
Linux Freak <vinu58@users.noreply.github.com>
2018-08-05 15:00:14 +0530

    updating with new codes
    Adding a method to call the script using def method

--- logread.py
+++ logread.py
@@ -12,7 +12,36 @@ with open( file, 'r' ) as f:
 	for line in f:
 		if keyword in line:
 			print line
+			
+#Another method
 
+def readfile():
+    try:
+        with open( file, 'r' ) as f:
+            for line in f:
+                if keyword in line:
+                    print line
+    
+    except:
+        print "Something went wrong"
+	
+print readfile()
 
-  
+#except IOError:
+#    print('An error occured trying to read the file.')
+    
+#except ValueError:
+#    print('Non-numeric data found in the file.')
+
+#except ImportError:
+#    print "NO module found"
+    
+#except EOFError:
+#    print('Why did you do an EOF on me?')
+
+#except KeyboardInterrupt:
+#    print('You cancelled the operation.')
+
+#except:
+#    print('An error occured.') 
 
